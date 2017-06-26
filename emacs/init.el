9b79012aa214c2bafc40f5b8a950b075df3a416a
pvpnvz <pvpnvz@users.noreply.github.com>
2017-06-26 21:47:47 +0800

    Add duplicate and comment line

--- emacs/init.el
+++ emacs/init.el
@@ -130,42 +130,31 @@
 ;;   '(backup-directory-alist '((".*" . "~/.emacscache/backups/"))))
 
 ;;;*duplicate line
-(defun duplicate-line (arg)
-  "Duplicate current line, leaving point in lower line."
-  (interactive "*p")
+(defun pliakas-duplicate-line (&optional commentfirst)
+  (interactive)
+  (beginning-of-line)
+  (push-mark)
+  (end-of-line)
+  (let ((str (buffer-substring
+	      (region-beginning)
+	      (region-end))))
+    (when commentfirst
+    (comment-region
+     (region-beginning)
+     (region-end)))
 
-  ;; save the point for undo
-  (setq buffer-undo-list (cons (point) buffer-undo-list))
+    (insert-string
+      (concat (if (= 0 (forward-line 1)) "" "\n")
+	      str "\n"))
+    (forward-line -1)))
 
-  ;; local variables for start and end of line
-  (let ((bol (save-excursion (beginning-of-line) (point)))
-        eol)
-    (save-excursion
+;; duplicate a line
+(global-set-key (kbd "C-x y") 'pliakas-duplicate-line)
 
-      ;; don't use forward-line for this, because you would have
-      ;; to check whether you are at the end of the buffer
-      (end-of-line)
-      (setq eol (point))
-
-      ;; store the line and disable the recording of undo information
-      (let ((line (buffer-substring bol eol))
-            (buffer-undo-list t)
-            (count arg))
-        ;; insert the line arg times
-        (while (> count 0)
-          (newline)         ;; because there is no newline in 'line'
-          (insert line)
-          (setq count (1- count)))
-        )
-
-      ;; create the undo information
-      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
-    ) ; end-of-let
-
-  ;; put the point in the lowest line and return
-  (next-line arg))
-
-(global-set-key (kbd "C-x y") 'duplicate-line)
+;; duplicate a line and comment the first line
+(global-set-key (kbd "C-x C-y")
+		(lambda()(interactive)
+		  (pliakas-duplicate-line t)))
 
 ;;;*org mode
 (add-to-list 'load-path "~/.emacs.d/plugins/org-9.0.8/lisp")
